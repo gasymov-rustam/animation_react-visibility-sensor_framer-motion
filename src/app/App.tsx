0318346b24bd4bd2bc2f5d6ba33e8726055f435a
Rustam Gasymov <rustam.gasymov@devalore.com>
2022-11-04 08:52:29 +0200

    add progress scroll bar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,32 @@
-import { memo } from 'react';
+import { CSSProperties, memo } from 'react';
+import { motion, useScroll } from 'framer-motion';
 import { Download, Experience, Footer, Header, Hero, Search } from '../components';
 
 export const App = memo(() => {
+  const { scrollYProgress } = useScroll();
+
+  const progress: CSSProperties = {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    height: 3,
+    backgroundImage: 'linear-gradient(to right, red, green, blue)',
+    transformOrigin: '0%',
+    zIndex: 1000,
+  };
+
   return (
-    <div className='text-white overflow-hidden'>
-      <Header />
-      <Hero />
-      <Experience />
-      <Search />
-      <Download />
-      <Footer />
-    </div>
+    <>
+      <motion.div style={{ ...progress, scaleX: scrollYProgress }} />
+      <div className='text-white overflow-hidden'>
+        <Header />
+        <Hero />
+        <Experience />
+        <Search />
+        <Download />
+        <Footer />
+      </div>
+    </>
   );
 });
